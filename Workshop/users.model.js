75cf564d8e488573c230f9acaf304d86f43dafa6
DanielSulea <66712560+DanielSulea@users.noreply.github.com>
2020-09-08 07:33:31 +0300

    Update users.model.js

--- Workshop/users.model.js
+++ Workshop/users.model.js
@@ -1,1 +1,9 @@
 // users
+
+class UsersModel {
+    apiUrl = 'http://jsonplaceholder.typicode.com/users/';
+
+    getUserById(id, options) {
+        return fetch(this.apiUrl + id, options).then(res => res.json());
+    }
+}
